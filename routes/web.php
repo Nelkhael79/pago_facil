d96fb89dfa9dcec7176661b770838f25b208b542
Nelkhael79 <samario79@gmail.com>
2020-02-26 21:37:14 -0500

    Arreglos Multidimensionales

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Ruta para arreglos multidimensionales
+Route::get('arreglosMD',function(){
+    $paises = [ "Colombia" => [
+                        "Capital :" => "Bogota D.C.",
+                        "Moneda " => "Peso",
+                        "Poblacion :" => 50
+                                ],  
+                "Peru" => [
+                    "Capital :" => "Lima",
+                    "Moneda " => "Sol",
+                    "Poblacion :" => 32
+                ],
+                "Bolivia" => [
+                    "Capital :" => "Sucre",
+                    "Moneda " => "Boliviano",
+                    "Poblacion :" => 12
+                ] 
+            ];
+                echo "<pre>";
+                var_dump($paises);
+                echo "</pre>";
+
+                //Recorrer el arreglo
+                foreach($paises as $pais => $informacion){
+                    echo "<h1> $pais:</h1>";
+                    foreach($informacion as $clave => $valor){
+                        echo "$clave: $valor", "<br/>";
+                    }
+                };
+    
+
+});
